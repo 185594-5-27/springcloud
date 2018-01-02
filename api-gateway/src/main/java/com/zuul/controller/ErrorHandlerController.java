d71102143f79f3fffef936dd26d0218d6f43c231
林泽锋 <282245889@qq.com>
2018-01-02 14:29:59 +0800

    增加网关通用错误格式的输出的逻辑

--- api-gateway/src/main/java/com/zuul/controller/ErrorHandlerController.java
+++ api-gateway/src/main/java/com/zuul/controller/ErrorHandlerController.java
@@ -1,12 +1,19 @@
 package com.zuul.controller;
 
 import com.netflix.zuul.context.RequestContext;
+import com.zuul.entity.ErrorException;
+import com.zuul.util.CombineException;
 import net.sf.json.JSONObject;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
 * 类描述：
@@ -23,13 +30,9 @@ public class ErrorHandlerController implements ErrorController {
     @RequestMapping("/error")
     public String error(HttpServletRequest request) {
         RequestContext ctx = RequestContext.getCurrentContext();
-        Throwable throwable = ctx.getThrowable();
-        JSONObject jobj = new JSONObject();
-        jobj.put("errorCode","-403");
-        jobj.put("msg","服务器请求超时");
-        System.out.println(throwable.getMessage());
-        System.out.println(ctx.get("error.exception"));
-        return jobj.toString();
+        return CombineException.getErrorException(ctx.getThrowable()).toString();
     }
+
+
 
 }
